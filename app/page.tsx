5bdcf0d304d6e5690ebf75afd013212b1cc03b42
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-06-27 18:11:57 +0100

    i wonder if i can do it

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Lander from "@/components/Lander";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       Hello World
-    </main>
+    <>
+      <Lander />
+    </>
   );
 }
---
+++ components/Lander.tsx
@@ -0,0 +1,25 @@
+import { Montserrat, Oswald, Ubuntu } from "next/font/google";
+
+const mont = Montserrat({ weight: ["600"], subsets: ["vietnamese"] });
+const oswald = Oswald({ weight: ["600"], subsets: ["vietnamese"] });
+const ubuntu = Ubuntu({ weight: ["500"], subsets: ["latin"] })
+export default function Lander() {
+  return (
+    <>
+      <div className="bg-[#071952]   before:-bottom-7 before:rounded-[50%] h-screen flex items-center justify-center lg:flex-row md:flex-row flex-col">
+        <div className={`text-[#EBF4F6] text-center ${oswald.className} px-10`}>
+          <h1 className={`text-4xl py-4 ${mont.className}`}>
+            Business Manager
+          </h1>
+          <p className={`py-4 ${ubuntu.className}`}>
+            Simplify your business operations with our intuitive platform.
+            Generate custom invoices for different companies and track your
+            earnings with ease. Stay on top of your financial game with our
+            comprehensive monitoring tools
+          </p>
+          <button className="bg-[#37B7C3] py-2 px-4"> Get Started</button>
+        </div>
+      </div>
+    </>
+  );
+}
