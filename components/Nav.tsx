f61ad5e962aac921b7aadaa4c5b9a47a7ed919cd
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-06-28 12:23:22 +0100

    start nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,22 @@
+import { Ubuntu } from "next/font/google";
+import Link from "next/link";
+
+const ubuntu = Ubuntu({weight:["500"], subsets:["latin"]})
 export default function Nav(){
     return (
         <>
+        <div>
+            <nav className={`flex items-center p-8 bg-[#071952] text-[#EBF4F6]  justify-between lg:flex-row md:flex-row ${ubuntu.className}`}>
+              <div>
+                <h2 className="text-xl">Business Manager</h2>
+              </div>
+              <div className="lg:flex gap-8 md:flex hidden">
+                <Link href={'/'}>Home</Link>
+                <Link href={'#about'}>About</Link>
+                <Link href={'/login'}>Login</Link>
+              </div>
+            </nav>
+        </div>
         </>
     )
 }
