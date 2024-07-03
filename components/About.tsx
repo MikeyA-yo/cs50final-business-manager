6a8654395b11f13411ca4e2600b5b1b3ded38464
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-07-03 19:36:14 +0100

    added width

--- components/About.tsx
+++ components/About.tsx
@@ -6,7 +6,7 @@ const mont = Montserrat({ weight: ["600"], subsets: ["vietnamese"] });
 export default function About() {
   return (
     <>
-      <div id="about" className="bg-[#071952] text-[#EBF4F6]">
+      <div id="about" className="bg-[#071952] lg:py-10 text-[#EBF4F6]">
         <div className="flex flex-col items-center justify-center text-center pt-10 gap-4">
           <h2 className="text-3xl">
             About <span className="text-[#088395]">Us</span>
@@ -20,6 +20,7 @@ export default function About() {
         <div className="flex items-center lg:flex-row flex-col gap-4 p-8 justify-center">
           <div>
             <Image
+             className=" h-96 w-96"
               src={`/about-img.png`}
               alt="about image"
               height={350}
@@ -33,7 +34,7 @@ export default function About() {
             make informed decisions, and drive growth.<br /> We offer an intuitive
             platform for entering and monitoring expenses and revenues, designed
             to be user-friendly and accessible.<br /> Whether you’re a small startup
-            or managing multiple clients, our solution provides detailed
+            or managing multiple clients,<br /> our solution provides detailed
             insights and real-time data to help you stay on top of your
             financial health.<br /> At Business Manager, we prioritize exceptional
             service and support, ensuring you get the most out of our platform.<br />
