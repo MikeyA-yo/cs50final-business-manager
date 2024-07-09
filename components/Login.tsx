5d8cb2dda47fa8f2f16a6c8e5d5c95cfa00ba6d2
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-07-09 22:23:27 +0100

    that's a wrap for now

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import GoogleSvg from "./google";
 import { Inter } from "next/font/google";
 import { auth } from "@/app/api/auth/auth";
+import Link from "next/link";
 
 
 const inter = Inter({weight:["500"], subsets:["vietnamese"]})
@@ -15,7 +16,7 @@ export default async function Login(){
             <Image src={`/finance.svg`} alt="random alt" width={400} height={400} />
         </div>
         <div className="h-screen w-full gap-2 flex items-center justify-center bg-[#EBF4F6] ">
-            <div className="bg-[#37B7C3] gap-2 p-2 shadow-lg shadow-[#088395] flex items-center justify-center"><GoogleSvg className="h-8 w-8" /> <span>Sign in With Google</span></div>
+            <Link href={`/api/auth/login`}><div className="bg-[#37B7C3] gap-2 p-2 shadow-lg shadow-[#088395] flex items-center justify-center"><GoogleSvg className="h-8 w-8" /> <span>Sign in With Google</span></div></Link>
         </div>
       </div>
     </>
