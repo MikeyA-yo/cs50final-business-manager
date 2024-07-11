85b907cf1edeaaa2b3a78fdaae61751627a68cc4
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-07-11 19:07:41 +0100

    this throws error but i'm leaving it for glow to see it

--- components/emailServerworkings.ts
+++ components/emailServerworkings.ts
@@ -1,7 +1,14 @@
 "use server";
 
+import { clientPromise } from "@/app/api/mongodb";
+import bcrypt from "bcrypt"
+import { lucia } from "@/app/api/auth/auth";
+import { cookies } from "next/headers";
+
+
 import { redirect } from "next/navigation";
 import { formdata } from "./emailsignin";
+import { isValidEmail } from "@/app/api/auth/login/email/functions";
 
 export async function CreateAccount(data: formdata) {
   const name = data.firstName;
@@ -83,24 +90,37 @@ export async function CreateAccountv2(
       };
     }
   } else if (email && password) {
-    const res = await fetch("http://localhost:3000/api/auth/login/email", {
-      method: "POST",
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    });
-
-    if (res.status === 302) {
-      redirect("/");
+    const cookie = cookies();
+    const client = await clientPromise;
+    const db = client.db("BusinessManager");
+    const users = db.collection("users");
+    if(!isValidEmail(email as string)){
+        return {
+          message:"Invalid Email"
+        }
     }
-
-    if (res.status === 400) {
-      const text = await res.text();
-      return {
-        message: text,
-      };
+    const existingUser = await users.findOne({email})
+    if(!existingUser){
+        return {
+          message:"User Doesn't exists"
+        }
     }
+    if(!existingUser.hashedPassword){
+        return {
+          message:"This account was created with google, please sign in with google"
+        }
+    }
+    const check = await bcrypt.compare(password as string, existingUser.hashedPassword);
+    if(check){
+        const session = await lucia.createSession(existingUser._id, {});
+        const sessionCookie = lucia.createSessionCookie(session.id);
+        console.log(sessionCookie)
+        cookie.set(
+            sessionCookie.name, 
+            sessionCookie.value, 
+            sessionCookie.attributes
+        );
+      }
   }
  return redirect("/");
 }
