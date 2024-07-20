8a61f968a4923ba119d033f3e0656bcdb79d9cba
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-07-20 19:11:46 +0100

    filler commit

--- components/dashboard/sideNav.tsx
+++ components/dashboard/sideNav.tsx
@@ -2,7 +2,11 @@ export default function SideNav() {
   return (
     <>
       <div className="flex flex-col min-h-screen bg-[#071952] p-10 items-center justify-center">
-          Ayo
+          <div className="bg-[#37B7C3]">
+
+          </div>
+          <div className="bg-[#37B7C3]"></div>
+          <div className="bg-[#37B7C3]"></div>
       </div>
     </>
   );
