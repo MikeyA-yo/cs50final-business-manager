7b0a3236d6a2b732b4021b54c070417412010bbc
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-07-22 09:41:54 +0100

    how good stuff starts

--- components/dashboard/sideNav.tsx
+++ components/dashboard/sideNav.tsx
@@ -1,10 +1,14 @@
+"use client"
 import Link from "next/link";
-import { Invoice, Signout, UserPlus } from "../someSvgs";
+import { DashBar, Invoice, Signout, UserPlus, XMark } from "../someSvgs";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function SideNav() {
+  const [open, setOpen] = useState(false)
   return (
     <>
-      <div className="lg:flex md:flex hidden flex-col min-h-screen bg-[#071952] p-4 items-center justify-between">
+      <div className="lg:flex md:flex hidden flex-col min-h-screen bg-[#071952] p-8 items-center justify-between">
         <div className="nav-dash flex gap-6 flex-col ">
           <div className="bg-[#37B7C3] rounded-lg p-2">
             <h3>Revenue</h3>
@@ -20,6 +24,21 @@ export default function SideNav() {
             <Link href={`/api/auth/signout`} className="flex gap-2">Sign Out <Signout className="size-6" /></Link>
         </div>
       </div>
+      <div className="lg:hidden md:hidden flex fixed z-10 text-[#EBF4F6]">
+        {!open && <div className="m-3 bg-[#071952] p-2 rounded-full" onClick={()=>setOpen(true)}><DashBar className="size-6" /></div>}
+        {open && <MobileNav onClick={()=>setOpen(false)} />}
+      </div>
     </>
   );
+}
+function MobileNav({onClick}:{onClick:React.MouseEventHandler<HTMLDivElement>}){
+  return (
+    <AnimatePresence>
+      <motion.div className="flex flex-col min-h-screen bg-[#071952] p-8  items-center justify-between">
+         <div onClick={onClick}>
+          <XMark />
+         </div>
+      </motion.div>
+    </AnimatePresence>
+  )
 }
--- components/someSvgs.tsx
+++ components/someSvgs.tsx
@@ -28,37 +28,76 @@ export function Spinner(props: React.SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
-export function Signout(props:React.SVGProps<SVGSVGElement>) {
+export function Signout(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
       <path d="M217.9 105.9l122.8 122.8c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9-18.7 0-33.9-15.2-33.9-33.9V320H32c-17.7 0-32-14.3-32-32v-64c0-17.7 14.3-32 32-32h128v-62.1c0-18.7 15.2-33.9 33.9-33.9 9 0 17.6 3.6 24 9.9zM352 416h64c17.7 0 32-14.3 32-32V128c0-17.7-14.3-32-32-32h-64c-17.7 0-32-14.3-32-32s14.3-32 32-32h64c53 0 96 43 96 96v256c0 53-43 96-96 96h-64c-17.7 0-32-14.3-32-32s14.3-32 32-32z" />
     </svg>
-  )
+  );
 }
-export function Invoice(props:React.SVGProps<SVGSVGElement>) {
+export function Invoice(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" {...props}>
       <path d="M64 0C28.7 0 0 28.7 0 64v384c0 35.3 28.7 64 64 64h256c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zm192 0v128h128L256 0zM64 80c0-8.8 7.2-16 16-16h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H80c-8.8 0-16-7.2-16-16zm0 64c0-8.8 7.2-16 16-16h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H80c-8.8 0-16-7.2-16-16zm128 72c8.8 0 16 7.2 16 16v17.3c8.5 1.2 16.7 3.1 24.1 5.1 8.5 2.3 13.6 11 11.3 19.6s-11 13.6-19.6 11.3c-11.1-3-22-5.2-32.1-5.3-8.4-.1-17.4 1.8-23.6 5.5-5.7 3.4-8.1 7.3-8.1 12.8 0 3.7 1.3 6.5 7.3 10.1 6.9 4.1 16.6 7.1 29.2 10.9l.5.1c11.3 3.4 25.3 7.6 36.3 14.6 12.1 7.6 22.4 19.7 22.7 38.2.3 19.3-9.6 33.3-22.9 41.6-7.7 4.8-16.4 7.6-25.1 9.1V440c0 8.8-7.2 16-16 16s-16-7.2-16-16v-17.8c-11.2-2.1-21.7-5.7-30.9-8.9-2.1-.7-4.2-1.4-6.2-2.1-8.4-2.8-12.9-11.9-10.1-20.2s11.9-12.9 20.2-10.1c2.5.8 4.8 1.6 7.1 2.4 13.6 4.6 24.6 8.4 36.3 8.7 9.1.3 17.9-1.7 23.7-5.3 5.1-3.2 7.9-7.3 7.8-14-.1-4.6-1.8-7.8-7.7-11.6-6.8-4.3-16.5-7.4-29-11.2l-1.6-.5c-11-3.3-24.3-7.3-34.8-13.7-12-7.2-22.6-18.9-22.7-37.3-.1-19.4 10.8-32.8 23.8-40.5 7.5-4.4 15.8-7.2 24.1-8.7v-17.3c0-8.8 7.2-16 16-16z" />
     </svg>
-  )
+  );
 }
 
-export function DashBar(props:React.SVGProps<SVGSVGElement>){
+export function DashBar(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth="{1.5}"
-    stroke="currentColor"
-    className="size-6"
-    {...props}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"
-    />
-  </svg>
-  )
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="{1.5}"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"
+      />
+    </svg>
+  );
+}
+
+export function XMark(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      {...props}
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  );
+}
+
+export function Wallet(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3"
+      />
+    </svg>
+  );
 }
