bd58c0a3ad9f9e794a8918d08e056c17df22c454
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-07-22 16:54:47 +0100

    added few change

--- components/dashboard/Charts.tsx
+++ components/dashboard/Charts.tsx
@@ -35,14 +35,14 @@ export default function Chart({dataSet}:{dataSet?:any}) {
   return (
     <>
       <ResponsiveContainer height={"100%"} width={"100%"}  minHeight={300} minWidth={150}>
-        <BarChart  data={data}>
+        <BarChart data={data}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
+          <Bar dataKey="pv" fill="#088395" />
+          <Bar dataKey="uv" fill="#37B7C3" />
         </BarChart>
       </ResponsiveContainer>
     </>
--- components/dashboard/dashboardMain.tsx
+++ components/dashboard/dashboardMain.tsx
@@ -4,10 +4,10 @@ export default function Dashboard() {
   return (
     <>
       <div className="min-h-screen flex lg:flex-row flex-col items-center justify-center bg-[#EBF4F6] w-full">
-        <div className="lg:w-96 md:w-80 flex lg:flex-row flex-wrap flex-col w-full">
+        <div className="lg:w-96 md:w-80 p-4 flex lg:flex-row flex-wrap flex-col w-full">
           <Chart />
         </div>
-        <div className="lg:w-96 md:w-80 flex lg:flex-row flex-wrap flex-col w-full">
+        <div className="lg:w-96 md:w-80 p-4 flex lg:flex-row flex-wrap flex-col w-full">
           {" "}
           <Chart />
         </div>
