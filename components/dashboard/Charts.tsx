b26c4c79dc4afdc7f7e72b9edee3121d18181712
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-07-22 14:27:42 +0100

    charts

---
+++ components/dashboard/Charts.tsx
@@ -0,0 +1,50 @@
+"use client";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+  },
+];
+export default function Chart() {
+  return (
+    <>
+      <ResponsiveContainer height={"100%"} width={"80%"}  minHeight={300} minWidth={150}>
+        <BarChart  data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
+  );
+}
--- components/dashboard/dashboardMain.tsx
+++ components/dashboard/dashboardMain.tsx
@@ -1,8 +1,13 @@
+import Chart from "./Charts";
+
 export default function Dashboard(){
     return (
         <>
-          <div>
+          <div className="min-h-screen flex items-center justify-center bg-[#EBF4F6] w-full">
            wow
+           <div>
+              <Chart />
+           </div>
           </div>
         </>
     )
