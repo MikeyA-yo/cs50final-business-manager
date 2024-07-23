c628e3ab0bea7e68cec13e762fc1f4d28c4a5fd2
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-07-23 15:36:15 +0100

    feels like i know nothing about businesses

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -9,6 +9,10 @@ export async function POST(req:Request){
     const businesses = db.collection("businesses");
     if (typeof name !== "string" ||typeof currency !== "string" ||typeof infrastructure !== "string" ||typeof motive !== "string" ||typeof role !== "string") return NextResponse.json({error:"Unknown DataTypes used"})
     try {
+        const business = await businesses.findOne({userId});
+        if(business){
+            return NextResponse.json({error:"This business is already registered"})
+        }
         await businesses.insertOne({name, motive, currency, role, infrastructure, userId})
         return NextResponse.json({message:'success'})
     } catch (e:any) {
--- components/dashboard/dashboardMain.tsx
+++ components/dashboard/dashboardMain.tsx
@@ -1,41 +1,60 @@
+import { auth } from "@/app/api/auth/auth";
 import Chart from "./Charts";
+import { redirect } from "next/navigation";
+import { clientPromise } from "@/app/api/mongodb";
 const defaultIncome = [
   {
-    name:"A",
-    in:8000,
-    out:3500
+    name: "A",
+    in: 8000,
+    out: 3500,
   },
   {
-    name:"B",
-    in:7500,
-    out:4500
+    name: "B",
+    in: 7500,
+    out: 4500,
   },
   {
-    name:"C",
-    in:6000,
-    out:6500
+    name: "C",
+    in: 6000,
+    out: 6500,
   },
   {
-    name:"D",
-    in:8000,
-    out:3500
+    name: "D",
+    in: 8000,
+    out: 3500,
+  },
+];
+export default async function Dashboard() {
+  const {user} = await auth();
+  if(!user){
+    return redirect("/login")
   }
-]
-export default function Dashboard() {
+  const client = await clientPromise;
+  const db = client.db("BusinessManager");
+  const businesses = db.collection("businesses");
+  const business = await businesses.findOne({userId: user.id.toString()});
+  if(!business){
+     return redirect("/register")
+  }
   return (
     <>
-      <div className="min-h-screen flex lg:flex-row flex-col items-center gap-2 justify-center bg-[#EBF4F6] w-full">
-        <div className="flex flex-col">
-          <h2 className="text-xl">Hey</h2>
-          <div className="lg:w-96 md:w-80 p-4 flex lg:flex-row flex-wrap flex-col w-full rounded border-[#071952] border">
-            <Chart dataSet={defaultIncome} />
-          </div>
+      <div className="min-h-screen flex flex-col items-center gap-2 justify-center bg-[#EBF4F6] w-full">
+        <div>
+          <h2 className="text-xl">{business?.name ?? "Your Business Name"} Analytics</h2>
         </div>
-        <div className="flex flex-col">
-          <h2 className="text-xl">Hey</h2>
-          <div className="lg:w-96 md:w-80 p-4 flex lg:flex-row flex-wrap flex-col w-full rounded border-[#071952] border">
-            {" "}
-            <Chart />
+        <div className="flex lg:flex-row lg:justify-around flex-col items-center gap-2 justify-center w-full">
+          <div className="flex flex-col">
+            <h2 className="text-xl">Expenditures</h2>
+            <div className="lg:w-96 md:w-80 p-4 flex lg:flex-row flex-wrap flex-col w-full rounded border-[#071952] border">
+              <Chart dataSet={defaultIncome} />
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <h2 className="text-xl">In and Outs</h2>
+            <div className="lg:w-96 md:w-80 p-4 flex lg:flex-row flex-wrap flex-col w-full rounded border-[#071952] border">
+              {" "}
+              <Chart />
+            </div>
           </div>
         </div>
       </div>
