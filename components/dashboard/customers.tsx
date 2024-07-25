864802307402a32f669721dc090f12904c95c221
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-07-25 11:56:30 +0100

    tables aren't responsive...

--- components/dashboard/customers.tsx
+++ components/dashboard/customers.tsx
@@ -26,13 +26,13 @@ export default async function Customers() {
         <CreateCustomer userID={user.id.toString()} />
         <div className="flex flex-col gap-2 items-center">
           <h2 className="text-3xl">Customers</h2>
-          <table>
+          <table className="lg:block md:block hidden">
             <tbody>
               {customersArray.length > 0 && (
                 <tr>
                   <th>Name</th>
                   <th>Email</th>
-                  <th>Invoices</th>
+                  <th>Total Invoices</th>
                 </tr>
               )}
               {customersArray.length > 0 &&
@@ -47,7 +47,16 @@ export default async function Customers() {
                 })}
             </tbody>
           </table>
-          {customersArray.length === 0 && <h3>No Customers Yet, create one to see them appear</h3>}
+          <div className="lg:hidden md:hidden flex flex-col items-center justify-evenly gap-2 p-4">
+             {customersArray.map((customer, i)=>{
+              return (
+                <CustomerCards name={customer.name ?? ""} email={customer.email ?? ""} key={i} />
+              )
+             })}
+          </div>
+          {customersArray.length === 0 && (
+            <h3>No Customers Yet, create one to see them appear</h3>
+          )}
         </div>
       </div>
     </>
@@ -59,7 +68,21 @@ function CustomerTable({ name, email }: { name: string; email: string }) {
     <tr className="even:bg-[#37B7C3]">
       <td className="tableData">{name}</td>
       <td className="tableData">{email}</td>
-      <td>None Yet</td>
+      <td className="tableData">0</td>
     </tr>
   );
+}
+
+function CustomerCards({ name, email }: { name: string; email: string }){
+  return (
+    <div className="flex flex-col justify-evenly w-52 gap-2">
+       <div className="border-b flex flex-col gap-1 p-2 border-gray-300">
+        <p>{name}</p>
+        <p>{email}</p>
+       </div>
+       <div className="border-b p-2 flex flex-col gap-1 border-gray-300">
+           0 invoices
+       </div>
+    </div>
+  )
 }
