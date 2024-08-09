a9e84fc0f4ea679619a8c962ec2ac36df8562425
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-08-09 16:42:28 +0100

    ridiculous

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer";
 import Lander from "@/components/Lander";
 import Services from "@/components/Services";
 import Why from "@/components/WhyUs";
-
+//error in my app not cause of my coding mistakes? this is ridiculous
 export default function Home() {
   return (
     <>
