58dad865c04540295af5dda888057bd89e468939
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-08-09 18:10:58 +0100

    nextjs could be the cause of all this

--- components/gmailLogin.tsx
+++ components/gmailLogin.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 import GoogleSvg from "./google";
 import { useState } from "react";
 import { Spinner } from "./someSvgs";
@@ -8,11 +7,11 @@ export default function GmailLogin(){
     const [load, setLoad] = useState(false)
     return (
         <>
-        <Link href={`/api/auth/login`} onClick={()=>setLoad(true)} className="lg:w-auto md:w-auto min-w-60">
+        <a href={`/api/auth/login`} onClick={()=>setLoad(true)} className="lg:w-auto md:w-auto min-w-60">
             <div className="bg-[#37B7C3] gap-2 p-2 shadow-lg shadow-[#088395] flex items-center justify-center">
               <GoogleSvg className="h-8 w-8" /> <span>Sign in With Google</span> {load && <Spinner className="animate-spin size-8" />}
             </div>
-          </Link>
+          </a>
         </>
     )
 }
